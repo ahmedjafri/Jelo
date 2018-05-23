ce50bb70bc529c2963b1f24c4d28585c03eb0312
Ahmed Jafri <ajafri@Ahmeds-Mac-Pro.fios-router.home>
2018-05-23 16:20:14 -0700

    Separate out ios and Android apps

--- index.android.js
+++ index.android.js
@@ -1,4 +1,31 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-AppRegistry.registerComponent('Jelo', () => App);
- 
+import { AppRegistry } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Amplify from "aws-amplify";
+import awsConfig from "./src/aws-exports";
+import Feed from "./components/Feed";
+
+Amplify.configure(awsConfig);
+
+import { withAuthenticator } from "aws-amplify-react-native";
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Feed />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 25,
+    backgroundColor: "#ecf0f1"
+  }
+});
+
+AppRegistry.registerComponent("Jelo", () => withAuthenticator(App));
--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,54 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-AppRegistry.registerComponent('Jelo', () => App);
+import { AppRegistry } from "react-native";
+import React from "react";
+import { NavigatorIOS, StyleSheet, View } from "react-native";
+import Amplify from "aws-amplify";
+import awsConfig from "./src/aws-exports";
+import Feed from "./components/Feed";
+import { withAuthenticator } from "aws-amplify-react-native";
+import PropTypes from "prop-types";
+
+class App extends React.Component {
+  render() {
+    return (
+      <NavigatorIOS
+        initialRoute={{
+          component: FeedPage,
+          title: "Feed"
+        }}
+        style={{ flex: 1 }}
+      />
+    );
+  }
+}
+
+class FeedPage extends React.Component {
+  static propTypes = {
+    navigator: PropTypes.object.isRequired
+  };
+
+  _onForward = () => {
+    this.props.navigator.push({
+      title: "Scene " + nextIndex
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Feed />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 25,
+    backgroundColor: "#ecf0f1"
+  }
+});
+
+Amplify.configure(awsConfig);
+AppRegistry.registerComponent("Jelo", () => withAuthenticator(App));
