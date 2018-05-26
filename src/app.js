87dda57a1f982021d6e5d28deda619a076a85516
Ahmed Jafri <ajafri@Ahmeds-Mac-Pro.fios-router.home>
2018-05-25 22:52:07 -0700

    Fix up navigation bar

--- src/app.js
+++ src/app.js
@@ -5,40 +5,19 @@ import { registerScreens } from "./screens";
 // screen related book keeping
 registerScreens();
 
-const tabs = [
-  {
-    label: "Navigation",
-    screen: "index.feedScreen",
-    icon: require("../img/list.png"),
-    title: "Navigation Types"
-  },
-  {
-    label: "Actions",
-    screen: "index.feedScreen",
-    icon: require("../img/swap.png"),
-    title: "Navigation Actions"
-  }
-];
-
 // this will start our app
-Navigation.startTabBasedApp({
-  tabs,
-  animationType: Platform.OS === "ios" ? "slide-down" : "fade",
-  tabsStyle: {
-    tabBarBackgroundColor: "#003a66",
-    tabBarButtonColor: "#ffffff",
-    tabBarSelectedButtonColor: "#ff505c",
-    tabFontFamily: "BioRhyme-Bold"
+Navigation.startSingleScreenApp({
+  screen: {
+    label: "Feed",
+    screen: "index.feedScreen",
+    title: "Feed",
+    navigatorButtons: {
+      rightButtons: [
+        {
+          title: "Camera"
+        }
+      ]
+    }
   },
-  appStyle: {
-    tabBarBackgroundColor: "#003a66",
-    navBarButtonColor: "#ffffff",
-    tabBarButtonColor: "#ffffff",
-    navBarTextColor: "#ffffff",
-    tabBarSelectedButtonColor: "#ff505c",
-    navigationBarColor: "#003a66",
-    navBarBackgroundColor: "#003a66",
-    statusBarColor: "#002b4c",
-    tabFontFamily: "BioRhyme-Bold"
-  }
+  animationType: Platform.OS === "ios" ? "slide-down" : "fade"
 });
